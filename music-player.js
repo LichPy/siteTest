f30348fb4a36ccec2b95416d7645bde1f15193a0
LichPy <lichpython@gmail.com>
2026-08-15 21:18:09 +1200

    Refactor music player code for consistency

--- music-player.js
+++ music-player.js
@@ -10,13 +10,13 @@ const nextSongButton = document.getElementById("next-song");
 
 const songs = [
     {
-        image: "./music/images/KISS.PNG",
+        image: "./music/KISS.PNG",
         name: "Kiss",
         artist: "Lil Peep",
         audio: "./music/songs/Kiss by Lil Peep.mp3"
     },
     {
-        image: "./music/images/EUPHORIA.PNG",
+        image: "./music/EUPHORIA.PNG",
         name: "Euphoria",
         artist: "Keshi",
         audio: "./music/songs/Euphoria by Keshi.mp3"
